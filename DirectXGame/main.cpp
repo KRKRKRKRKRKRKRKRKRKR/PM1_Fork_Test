c831852bd45f1e40678d2a796e15030de764795a
KRKRKRKRKRKRKRKRKRKR <k025g0083@g.neec.ac.jp>
2026-02-10 12:32:52 +0900

    test

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,5 +4,10 @@
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	KamataEngine::Initialize();
+	while (1) {
+	
+	}
+
 	return 0;
+
 }
